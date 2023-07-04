b08c8b9254c8402667ccaf1e547276371a3bd5e8
Nicolae-Marian Gartu <gartu.marian1973@gmail.com>
2023-07-04 14:26:37 +0100

    Implement skeleton of game logic

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var Gameboard = (() => {
 	var gameboard = ["", "", "", "", "", "", "", "", ""];
 
-	const contentElement = document.getElementsByClassName("content")[0];
 	const gameboardElement = document.getElementsByClassName("gameboard")[0];
 	const gameboardCells = document.getElementsByClassName("gameboard__cell");
 
@@ -11,23 +10,67 @@ var Gameboard = (() => {
 		}
 	}
 
-	function placeSymbolAt(index) {
-		gameboard[index] = "X";
+	function placeSymbolAt(index, symbol) {
+		gameboard[index] = symbol;
 		_render();
 	}
 
-	for (let i = 0; i < gameboardCells.length; i++) {
-		gameboardCell = gameboardCells[i];
-		gameboardCell.addEventListener(
-			"click",
-			function () {
-				placeSymbolAt(i);
-			},
-			true
-		);
+	function setCurrentSymbol(symbol) {
+		for (let i = 0; i < gameboardCells.length; i++) {
+			gameboardCell = gameboardCells[i];
+			gameboardCell.addEventListener(
+				"click",
+				function () {
+					placeSymbolAt(i, symbol);
+				},
+				true
+			);
+		}
 	}
 
 	_render();
 
-	return { placeSymbolAt };
+	return { placeSymbolAt, setCurrentSymbol };
 })();
+
+var GameController = (() => {
+	let players = [];
+	let playerScores = [];
+	let turnNumber = 0;
+
+	function registerPlayer(name, symbol) {
+		players.push(Player(name, symbol));
+		playerScores.push(0);
+	}
+
+	function _getCurrentPlayerIndex() {
+		return turnNumber % 2;
+	}
+
+	function _initGame() {
+		registerPlayer("Player One", "X");
+		registerPlayer("Player Two", "O");
+	}
+
+	function _isGameFinished() {
+		return;
+	}
+
+	function playRound() {
+		_initGame();
+		Gameboard.setCurrentSymbol(players[_getCurrentPlayerIndex()].symbol);
+		turnNumber++;
+		// while (!_isGameFinished()) {
+		// }
+	}
+	return { playRound };
+})();
+
+const Player = (name, symbol) => {
+	return {
+		name,
+		symbol,
+	};
+};
+
+GameController.playRound();
